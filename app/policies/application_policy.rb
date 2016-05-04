2d76997bfab8e7e7ab09a9ab428e755d1ee243e5
Michael Quaas <michael.quaas@betterplace.org>
2016-05-04 17:36:59 +0200

    devise views

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,58 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    admin?
+  end
+
+  def show?
+    admin?
+  end
+
+  def create?
+    admin?
+  end
+
+  def new?
+    admin?
+  end
+
+  def update?
+    admin?
+  end
+
+  def edit?
+    admin?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  # def scope
+  #   Pundit.policy_scope!(user, record.class)
+  # end
+
+  def admin?
+    user.role == 'admin'
+  end
+
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+end
