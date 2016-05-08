7912273142728fc448a58ca7e7235e7a22ee1d3a
Michael Quaas <michael.quaas@betterplace.org>
2016-05-08 22:08:24 +0200

    importer: some helper methods

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,18 +1,31 @@
 module Importer
+
+  def self.node_to_hash(node)
+    node.children.select(&:element?).each_with_object({}) do |value_node, hsh|
+      hsh[value_node.name] = value_node.text
+    end
+  end
+
+  def self.reset(model)
+    model.delete_all
+    ActiveRecord::Base.connection.reset_pk_sequence!(model.table_name)
+  end
+
   def self.import_customers(with_delete: false)
 
-    if with_delete
-      Customer.delete_all
-      ActiveRecord::Base.connection.reset_pk_sequence!(Customer.table_name)
-    end
+    reset(Customer) if with_delete
 
     doc = Nokogiri::XML(File.open(Rails.root.join('db', 'seeds','customers.xml')))
-    doc.xpath('//customers').each do |customer_node|
-      data_nodes = customer_node.children.select(&:element?)
-      customer_hash = data_nodes.each_with_object({}) do |child_node, hsh|
-        hsh[child_node.name] = child_node.text
-      end
-      Customer.create! customer_hash
+    doc.xpath('//customers').each { |customer| Customer.create! node_to_hash(customer) }
+  end
+
+  def self.import_deliveries(with_delete: false)
+
+    # reset(Delivery) if with_delete
+
+    doc = Nokogiri::XML(File.open(Rails.root.join('db', 'seeds','deliveries.xml')))
+    doc.xpath('//deliveries[1]').each do |delivery|
+      # Delivery.create node_to_hash(delivery)
     end
   end
 end
