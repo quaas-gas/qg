d10dc0a1c083784ce5c3cb6078673357c32abd51
Michael Quaas <michael.quaas@betterplace.org>
2016-05-10 22:46:55 +0200

    remove old migrations

--- db/migrate/20160505120005_add_mobile_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddMobileToCustomers < ActiveRecord::Migration
-  def change
-    add_column :customers, :mobile, :string
-  end
-end
--- db/migrate/20160506182736_add_archived_to_customer.rb
+++
@@ -1,5 +0,0 @@
-class AddArchivedToCustomer < ActiveRecord::Migration
-  def change
-    add_column :customers, :archived, :boolean, default: false
-  end
-end
