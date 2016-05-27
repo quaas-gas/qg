fc658e9614b8214521fec83c89b0ad55389342de
Michael Quaas <michael.quaas@betterplace.org>
2016-05-27 12:30:21 +0200

    db schema changed after fk edition

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519055642) do
+ActiveRecord::Schema.define(version: 20160527101638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -206,7 +206,7 @@ ActiveRecord::Schema.define(version: 20160519055642) do
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "deliveries", "customers"
-  add_foreign_key "delivery_items", "deliveries"
+  add_foreign_key "delivery_items", "deliveries", on_delete: :cascade
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoices", "customers"
   add_foreign_key "prices", "customers"
