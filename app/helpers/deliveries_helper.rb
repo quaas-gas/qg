455a754ea18df60cc8230c48f9c8e352cd3ac7a3
Michael Quaas <michael.quaas@betterplace.org>
2016-06-02 14:41:23 +0200

    customer view simplified

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -1,2 +1,12 @@
 module DeliveriesHelper
+
+  def format_delivery_item_count(item)
+    return '' unless item
+    diff      = item.count - item.count_back
+    css_class = { -1 => 'text-danger', 1 => 'text-success' }[diff <=> 0]
+    res       = "#{item.count} / #{item.count_back}"
+    diff_text = diff == 0 ? '-' : diff
+
+    content_tag(:span, "#{res} #{diff_text}", class: css_class).html_safe
+  end
 end
