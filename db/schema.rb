3e4f2d193ef90360383ac36baea90f9b089b3e74
Michael Quaas <michael.quaas@betterplace.org>
2016-06-03 16:36:15 +0200

    add notes to customers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602201553) do
+ActiveRecord::Schema.define(version: 20160603142401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160602201553) do
     t.boolean  "archived",        default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.text     "notes"
   end
 
   create_table "deliveries", force: :cascade do |t|
