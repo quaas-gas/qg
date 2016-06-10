b7065d7e36423b83cfc201d9fe07ea995b8218db
Michael Quaas <michael.quaas@betterplace.org>
2016-06-10 12:26:50 +0200

    search also id of customer

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,7 +2,7 @@ module SearchHelper
   def display_name_for(result)
     case result
     when Customer
-      [result.salut, result.name, result.name2, result.city].select(&:present?).join(', ')
+      [result.id, result.salut, result.name, result.name2, result.city].select(&:present?).join(', ')
     when Delivery
       [result.number, ldate(result.date), result.customer.name].select(&:present?).join(', ')
     when Invoice
