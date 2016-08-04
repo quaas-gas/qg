0a0289d96f8c5a697e63d73aef7853f8845a44f9
Michael Quaas <michael.quaas@betterplace.org>
2016-08-04 03:03:30 +0200

    refactoring of Statistic

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,47 +1,32 @@
 class Statistic < ActiveRecord::Base
   validates :name, presence: true
 
-  attr_reader :result, :y_sums
+  attr_reader :result
 
-  TIME_RANGES = %w(this_week last_week this_month last_month this_year last_year custom)
+  TIME_RANGES = %w(week month quart year).flat_map do |period|
+    %w(this last).map { |this_last| "#{this_last}_#{period}" }
+  end
+  TIME_RANGES += ['custom']
   GROUPING    = %w(region customer_category product_category time seller)
+  FILTERS     = %w(regions customer_categories product_categories)
   SUMS        = %w(content net tax)
+  SUM_TYPES   = %i(content net tax net_content)
 
   def time_range_relative
     time_range['relative']
   end
 
-  def grouping_x
-    grouping['x']
-  end
+  %w(x y).each { |dim| define_method("grouping_#{dim}") { grouping[dim] } }
 
-  def grouping_y
-    grouping['y']
-  end
-
-  def regions
-    filter['regions'] || []
-  end
-
-  def customer_categories
-    filter['customer_categories'] || []
-  end
-
-  def product_categories
-    filter['product_categories'] || []
-  end
+  FILTERS.each { |filter_name| define_method(filter_name) { filter[filter_name] || [] } }
 
   def labels_for(x_or_y)
-    case grouping[x_or_y.to_s]
-    when 'region'
-      regions.any? ? regions : Setting.customer_regions
-    when 'customer_category'
-      customer_categories.any? ? customer_categories : Setting.customer_categories
-    when 'product_category'
-      product_categories.any? ? product_categories : Setting.product_categories
-    when 'time'
-      (1..12).to_a
-    end
+    hash = (x_or_y == :x) ? @result[:content] : @result
+    hash.keys.reject { |key| key.is_a? Symbol }.sort
+  end
+
+  def label_for(x_or_y, label)
+    grouping[x_or_y.to_s] == 'time' ? I18n.t('date.abbr_month_names')[label] : label
   end
 
   def items_scope(only: nil)
@@ -67,73 +52,85 @@ class Statistic < ActiveRecord::Base
   end
 
   def calculate!
-    @result = {}
-    @y_sums = {}
-    sums = items_scope.sum(sum_option)
-    sums.each do |(first, second), value|
-      @result[first] ||= {}
-      second = second.to_i if grouping_x == 'time' # grouping by month returns float as key
-      @result[first][second] = cast_val value
+    @result = sums_for(:x)
+    items_scope.sum(sum_option).each do |(y, x), value|
+      @result[y] ||= {}
+      @result[y][x] = cast_val value
     end
-    @result[:total] = items_scope(only: :x).sum(sum_option)
-    if grouping_x == 'time' # grouping by month returns float as key
-      @result[:total].keys.each { |key| @result[:total][key.to_i] = @result[:total].delete key }
+    y_sums = sums_for(:y)
+    @result.each do |y_label, row|
+      SUM_TYPES.each { |sum_type| row[sum_type] = y_sums[sum_type][y_label] }
     end
-    @result[:total].each { |label, value| @result[:total][label] = cast_val value }
-    @y_sums[:content] = items_scope(only: :y).sum('count * products.content')
-    @y_sums[:net]     = items_scope(only: :y).sum('count * unit_price_cents')
-    @y_sums[:content][:total] = @y_sums[:content].values.sum
-    @y_sums[:net][:total] = @y_sums[:net].values.sum
     @result
   end
 
-  def cast_val(value)
-    case sums_of
-      when 'content' then value
-      when 'net' then Money.new(value).exchange_to('EURNET')
-      when 'tax' then Money.new(value).exchange_to('EU4TAX').exchange_to('EURTAX')
+  def sums_for(x_or_y)
+    sums = {
+      content:     items_scope(only: x_or_y).sum(sum_option(type: 'content')),
+      net:         items_scope(only: x_or_y).sum(sum_option(type: 'tax')),
+      tax:         {},
+      net_content: {},
+    }
+    sums[:net].each do |label, value|
+      sums[:net][label] = cast_val value, type: 'net'
+      sums[:tax][label] = cast_val value, type: 'tax'
+      sums[:net_content][label] = (sums[:net][label] / sums[:content][label]) if sums[:content][label] != 0
     end
+    %i(content net tax).each { |sum_type| sums[sum_type][:total] = sums[sum_type].values.sum }
+    sums
+  end
+
+  def cast_val(value, type: sums_of)
+    return value if type == 'content'
+    value = Money.new(value)
+    return value.exchange_to('EURNET') if type == 'net'
+    value.exchange_to('EU4TAX').exchange_to('EURTAX')
   end
 
   def group_option(x_or_y)
     case grouping[x_or_y.to_s]
-      when 'region' then 'customers.region'
+      when 'region'            then 'customers.region'
       when 'customer_category' then 'customers.category'
-      when 'product_category' then 'products.category'
-      when 'time' then 'extract(month from deliveries.date)'
-      when 'seller' then 'sellers.short'
+      when 'product_category'  then 'products.category'
+      when 'time'              then 'extract(month from deliveries.date)'
+      when 'seller'            then 'sellers.short'
     end
   end
 
-  def sum_option
-    case sums_of
-      when 'content' then 'count * products.content'
-      when 'net', 'tax' then 'count * unit_price_cents'
-    end
+  def sum_option(type: sums_of)
+    return 'count * products.content' if type == 'content'
+    'count * unit_price_cents'
   end
 
   def start_date
-    case time_range_relative
-      when 'this_week'  then Date.current.beginning_of_week
-      when 'last_week'  then 1.week.ago.to_date.beginning_of_week
-      when 'this_month' then Date.current.beginning_of_month
-      when 'last_month' then 1.month.ago.to_date.beginning_of_month
-      when 'this_year'  then Date.current.beginning_of_year
-      when 'last_year'  then 1.year.ago.to_date.beginning_of_year
-      when 'custom'     then Date.parse time_range['start_date']
-    end
+    date_range.first
   end
 
   def end_date
+    date_range.last
+  end
+
+  def date_range
+    return [custom_date('start'), custom_date('end')] if time_range_relative == 'custom'
+    today      = Date.current
+    last_week  = 1.week.ago.to_date
+    last_month = 1.month.ago.to_date
+    last_quart = 3.month.ago.to_date
+    last_year  = 1.year.ago.to_date
     case time_range_relative
-      when 'this_week'  then Date.current.end_of_week
-      when 'last_week'  then 1.week.ago.to_date.end_of_week
-      when 'this_month' then Date.current.end_of_month
-      when 'last_month' then 1.month.ago.to_date.end_of_month
-      when 'this_year'  then Date.current.end_of_year
-      when 'last_year'  then 1.year.ago.to_date.end_of_year
-      when 'custom'     then Date.parse time_range['end_date']
+      when 'this_week'  then [today.     beginning_of_week,    today.     end_of_week]
+      when 'last_week'  then [last_week. beginning_of_week,    last_week. end_of_week]
+      when 'this_month' then [today.     beginning_of_month,   today.     end_of_month]
+      when 'last_month' then [last_month.beginning_of_month,   last_month.end_of_month]
+      when 'this_quart' then [today.     beginning_of_quarter, today.     end_of_quarter]
+      when 'last_quart' then [last_quart.beginning_of_quarter, last_quart.end_of_quarter]
+      when 'this_year'  then [today.     beginning_of_year,    today.     end_of_year]
+      when 'last_year'  then [last_year. beginning_of_year,    last_year. end_of_year]
     end
+  end
+
+  def custom_date(start_or_end)
+    Date.parse(time_range["#{start_or_end}_date"])
   end
 
 end
