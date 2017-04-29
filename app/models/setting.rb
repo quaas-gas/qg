351ec1f3247c327baad8560d07837a51c5b536fe
Michael Quaas <michael.quaas@betterplace.org>
2017-04-29 14:21:42 +0200

    Setting.product_categories!

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,9 +3,9 @@ class Setting < RailsSettings::Base
   source Rails.root.join('config/app.yml')
   namespace Rails.env
 
-  # def self.product_categories!
-  #   self.product_categories = Product.uniq.pluck(:category).sort
-  # end
+  def self.product_categories!
+    self.product_categories = Product.uniq.pluck(:category).sort
+  end
 
   def self.customer_categories!
     self.customer_categories = Customer.uniq.pluck(:category).sort
